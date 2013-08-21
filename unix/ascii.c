9e39f8b8eb4988c15ef4efcebad89819ef1dd3e2
Manish Raghavan <manishraghavan@gmail.com>
2013-08-21 01:23:59 -0700

    Show line numbers in ascii

--- unix/ascii.c
+++ unix/ascii.c
@@ -4,14 +4,21 @@
 
 int main(int argc, char *argv[]) {
   FILE *fp;
-  int c;
+  int c, lnum;
+  char lnum_needed = 1;
   while (--argc > 0) {
+    lnum = 0;
     fp = fopen(*++argv, "r");
     printf("%s:\n", *argv);
     while ((c = fgetc(fp)) != EOF) {
+      if (lnum_needed) {
+        printf("%3d: ", lnum++);
+        lnum_needed = 0;
+      }
       printf("0x%02x ", c);
       if (c == '\n') {
         printf("\n");
+        lnum_needed = 1;
       }
     }
     printf("\n");

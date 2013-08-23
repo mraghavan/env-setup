ed10fbba94efbdba5e7baa7b714708adb3dfbf07
Manish Raghavan <manishraghavan@gmail.com>
2013-08-22 19:56:51 -0700

    Added line num, formatting options

--- unix/ascii.c
+++ unix/ascii.c
@@ -1,24 +1,50 @@
 /* ascii.c */
 
 #include <stdio.h>
+#include <stdlib.h>
+#define LNON 01
+#define STARTLINE 02
 
 int main(int argc, char *argv[]) {
+  char *DEC = "%02d ", *OCT = "0%02o ", *HEX = "0x%02x ";
+  char linenum = STARTLINE, *prog_name = argv[0];
+  char *format = DEC;
   FILE *fp;
   int c, lnum;
-  char lnum_needed = 1;
-  while (--argc > 0) {
-    lnum = 0;
-    fp = fopen(*++argv, "r");
-    printf("%s:\n", *argv);
-    while ((c = fgetc(fp)) != EOF) {
-      if (lnum_needed) {
-        printf("%3d: ", lnum++);
-        lnum_needed = 0;
+  while (--argc > 0 && (*++argv)[0] == '-') {
+    while (c = *++(argv[0])) {
+      switch (c) {
+        case 'l':
+          linenum |= LNON;
+          break;
+        case 'd':
+          format = DEC;
+          break;
+        case 'o':
+          format = OCT;
+          break;
+        case 'x':
+          format = HEX;
+          break;
+        default:
+          fprintf(stderr, "%s: illegal option %c\n", prog_name, c);
+          exit(1);
       }
-      printf("0x%02x ", c);
+    }
+  }
+  while (argc-- > 0) {
+    lnum = 0;
+    fp = fopen(*argv, "r");
+    printf("%s:\n", *argv++);
+    while ((c = fgetc(fp)) != EOF) {
+      if ((linenum & STARTLINE) && (linenum & LNON)) {
+        printf("%3d: ", lnum++);
+        linenum &= ~STARTLINE;
+      }
+      printf(format, c);
       if (c == '\n') {
         printf("\n");
-        lnum_needed = 1;
+        linenum |= STARTLINE;
       }
     }
     printf("\n");
